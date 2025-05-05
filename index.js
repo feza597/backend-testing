a802dff61d474dd8857c776edb2206dbd4fd132c
feza597 <feza@navaxis.ai>
2025-05-05 17:45:41 +1000

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 app.use(express.static('NavDev/dist'))
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 5000
 
 app.listen(port, () =>{
     console.log(`listening on port ${port}`);
